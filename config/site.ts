e2a269b61d670ebdff01926071b35ef98589293d
leichain <leilani-ledingham@hotmail.com>
2024-02-08 13:27:34 -0800

    title change

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "GoldRush Wallet and Portfolio UI",
+  name: "Demo.eth Wallet and Portfolio UI",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
